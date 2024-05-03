d3a483ec8841de01275ccb2c9a96f348476ac477
sendurj <166694939+sendurj@users.noreply.github.com>
2024-05-03 19:58:50 +0530

    Update time_zone.ps1

--- Post_Provisioning/roles/time_zone/files/time_zone.ps1
+++ Post_Provisioning/roles/time_zone/files/time_zone.ps1
@@ -1,3 +1,17 @@
+#Output file variables
+Function write-log{
+Param (
+[string] $msg
+)
+$date = Get-Date -Format "MM-dd-yyyy"
+New-Item -Path "C:\Programdata\Logs" -ItemType Directory -ErrorAction SilentlyContinue
+$Logfile = "C:\Programdata\Logs\Post-Build_Script1.log"
+$TimeStamp = Get-Date -Format "MM/dd/yyyy hh:mm:ss tt"
+write-host "$($TimeStamp): $msg"
+Write-Output "$($TimeStamp): $msg" |out-file $Logfile -append
+}
+
+#Timezone configuration
 param (
     [string]$Timezone
 )
