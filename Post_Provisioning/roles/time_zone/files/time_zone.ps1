1cc80b63589b1994e57138fb78979f900cf3c5d5
sendurj <166694939+sendurj@users.noreply.github.com>
2024-05-05 22:25:03 +0530

    Update time_zone.ps1

--- Post_Provisioning/roles/time_zone/files/time_zone.ps1
+++ Post_Provisioning/roles/time_zone/files/time_zone.ps1
@@ -1,20 +1,8 @@
-#Output file variables
-Function write-log{
-Param (
-[string] $msg
-)
-$date = Get-Date -Format "MM-dd-yyyy"
-New-Item -Path "C:\Programdata\Logs" -ItemType Directory -ErrorAction SilentlyContinue
-$Logfile = "C:\Programdata\Logs\Post-Build_Script1.log"
-$TimeStamp = Get-Date -Format "MM/dd/yyyy hh:mm:ss tt"
-write-host "$($TimeStamp): $msg"
-Write-Output "$($TimeStamp): $msg" |out-file $Logfile -append
-}
-
-$TZ = Get-TimeZone
-if ($TZ.id -ne "{{ input_timezone }}") {
-    Set-TimeZone -Name "{{ input_timezone }}"
-    Write-Host "Timezone set"
-} else {
-    Write-Host "Timezone already set"
-}
+$timezoneValue = "{{ input_timezone }}"
+    $TZ = Get-TimeZone
+    if ($TZ.Id -ne $timezoneValue) {
+        Set-TimeZone -Name $timezoneValue
+        Write-Host "Timezone set to $timezoneValue"
+    } else {
+        Write-Host "Timezone already set to $timezoneValue"
+    }
