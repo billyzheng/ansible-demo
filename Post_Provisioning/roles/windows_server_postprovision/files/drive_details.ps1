ae1cc7d6926704d81e9da7342214a34ed6c4633d
sendurj <166694939+sendurj@users.noreply.github.com>
2024-05-07 18:29:22 +0530

    Delete Post_Provisioning/roles/windows_server_postprovision/files/drive_details.ps1

--- Post_Provisioning/roles/windows_server_postprovision/files/drive_details.ps1
+++
@@ -1,5 +0,0 @@
-# Get the list of drives
-$drives = Get-WmiObject Win32_LogicalDisk | Select-Object DeviceID, VolumeName, Size, FreeSpace, FileSystem
-
-# Output the list of drives
-$drives | ConvertTo-Json
