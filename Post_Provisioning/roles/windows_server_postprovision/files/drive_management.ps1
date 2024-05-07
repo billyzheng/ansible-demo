83f10572024704de4d397cc6a654b9ad67e41ac4
sendurj <166694939+sendurj@users.noreply.github.com>
2024-05-07 19:51:57 +0530

    Update drive_management.ps1

--- Post_Provisioning/roles/windows_server_postprovision/files/drive_management.ps1
+++ Post_Provisioning/roles/windows_server_postprovision/files/drive_management.ps1
@@ -1,4 +1,3 @@
-﻿
 #Output file variables
 Function write-log{
 Param (
@@ -13,7 +12,50 @@ Write-Output "$($TimeStamp): $msg" |out-file $Logfile -append
 }
 
 #Change the CD drive letter
-$CDDrive = Get-WmiObject win32_volume -Filter 'DriveType = "5"'
-$CDDrive.DriveLetter = "z:"
-$CDDrive.put()
-write-log "CD drive letter changed to Z"
+try {
+    $CDDrive = Get-WmiObject win32_volume -Filter 'DriveType = "5"'
+
+    if ($CDDrive) {
+        $CDDrive.DriveLetter = "Z:"
+        $CDDrive.put()
+        Write-Host "CD drive letter changed to Z"
+        Write-Log "CD drive letter changed to Z"
+    } else {
+        Write-Host "No CD drive found"
+        Write-Log "No CD drive found"
+    }
+} catch {
+    Write-Host "An error occurred: $_"
+    Write-Log "An error occurred: $_"
+}
+
+#Expand C drive
+try {
+    $CDL = "C"
+    $CSize = (Get-PartitionSupportedSize -DriveLetter $CDL)
+    Resize-Partition -DriveLetter $CDL -Size $CSize.SizeMax
+    Write-Host "C drive expanded successfully"
+} catch {
+    Write-Host "Error: $_"
+}
+
+#Formatting available disks
+Function Partition-disk
+{
+ $newdisks = get-disk |where partitionstyle -eq 'raw'
+ If ($newdisks -ne $null)
+ {
+   foreach($newdisk in $newdisks)
+  {
+    $dl= get-Disk -number $newdisk.Number | Initialize-Disk -PartitionStyle GPT -PassThru |  New-Partition -AssignDriveLetter -UseMaximumSize
+    Format-Volume  -driveletter $dl.driveletter -FileSystem NTFS  -Confirm:$false
+	write-log "Disk $newdisk.number partitioned, formatted and assigned the drive letter as $dl.driveletter"
+  }
+ }
+ else
+ {
+  write-log "No raw disks found"
+ }
+}
+
+Partition-disk
