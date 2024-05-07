9afed840c5028a1d8b6322d813e64df63b7f9be3
sendurj <166694939+sendurj@users.noreply.github.com>
2024-05-07 18:33:20 +0530

    Add files via upload

---
+++ Post_Provisioning/roles/windows_server_postprovision/files/drive_management.ps1
@@ -0,0 +1,19 @@
+﻿
+#Output file variables
+Function write-log{
+Param (
+[string] $msg
+)
+$date = Get-Date -Format "MM-dd-yyyy"
+New-Item -Path "C:\Programdata\Logs" -ItemType Directory -ErrorAction SilentlyContinue
+$Logfile = "C:\Programdata\Logs\Post-Build_Script1.log"
+$TimeStamp = Get-Date -Format "MM/dd/yyyy hh:mm:ss tt"
+write-host "$($TimeStamp): $msg"
+Write-Output "$($TimeStamp): $msg" |out-file $Logfile -append
+}
+
+#Change the CD drive letter
+$CDDrive = Get-WmiObject win32_volume -Filter 'DriveType = "5"'
+$CDDrive.DriveLetter = "z:"
+$CDDrive.put()
+write-log "CD drive letter changed to Z"
