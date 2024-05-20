66db46eab1b78579eb029f14c151ba3cf3bad721
Rone <m13552079019@163.com>
2024-05-20 15:20:28 +0800

    add ignore errors for Guest

--- comparison.py
+++ comparison.py
@@ -1,4 +1,3 @@
-import os
 import re
 
 def get_control_ids(file_path):
@@ -15,37 +14,39 @@ def get_control_ids(file_path):
                     control_ids.append(number)
     return control_ids
 
-# file = open("C:/Users/CNROCHE33/Desktop/Rone/Ansible/ansible-demo/Windows/roles/windows_baseline_common/vars/main.yml", "r")
-# lines = file.readlines()
-# control_ids = []
+def read_all_control_ids(file_path):
+    file = open(file_path, "r")
+    lines = file.readlines()
+    control_ids = []
 
-# for line in lines:
-#     match = re.search(r'\bwin_baseline_(\d+)_state', line)
-#     if match:
-#         number = match.group(1)  # 提取捕获组中的数字
-#         if number.isdigit():
-#             if number not in control_ids:
-#                 control_ids.append(number)
+    for number in lines:
+        if number not in control_ids:
+            control_ids.append(number.strip())
+    return control_ids
 
-common_path = "C:/Users/CNROCHE33/Desktop/Rone/Ansible/ansible-demo/Windows/roles/windows_baseline_common/vars/main.yml"
-server_2019_path = "C:/Users/CNROCHE33/Desktop/Rone/Ansible/ansible-demo/Windows/roles/windows_baseline_2019/vars/main.yml"
-server_2022_path = "C:/Users/CNROCHE33/Desktop/Rone/Ansible/ansible-demo/Windows/roles/windows_baseline_2022/vars/main.yml"
-server_2016_path = "C:/Users/CNROCHE33/Desktop/Rone/Ansible/ansible-demo/Windows/roles/windows_baseline_2016/vars/main.yml"
+common_path = "C:/Users/CNROCHE33/Desktop/Rone/Ansible/ansible-demo/Windows/roles/windows_baseline_common/tasks/main.yml"
+server_2019_path = "C:/Users/CNROCHE33/Desktop/Rone/Ansible/ansible-demo/Windows/roles/windows_baseline_2019/tasks/main.yml"
+server_2022_path = "C:/Users/CNROCHE33/Desktop/Rone/Ansible/ansible-demo/Windows/roles/windows_baseline_2022/tasks/main.yml"
+server_2016_path = "C:/Users/CNROCHE33/Desktop/Rone/Ansible/ansible-demo/Windows/roles/windows_baseline_2016/tasks/main.yml"
+all_control_ids = "C:/Users/CNROCHE33/Desktop/Rone/Ansible/ansible-demo/Andrews_baselines_CID"
 
 common_ids = get_control_ids(common_path)
-print(len(common_ids))
-
 server_2019_ids = get_control_ids(server_2019_path)
-print(len(server_2019_ids))
-
 server_2022_ids = get_control_ids(server_2022_path)
-print(len(server_2022_ids))
-
 server_2016_ids = get_control_ids(server_2016_path)
-print(len(server_2016_ids))
-
 common_ids += server_2019_ids
 common_ids += server_2022_ids
 common_ids += server_2016_ids
 
-print(len(common_ids))
+all_ids = read_all_control_ids(all_control_ids)
+
+all_ids_set = set(all_ids)
+all_implemented_ids_set = set(common_ids)
+
+print(f'All control IDs: {len(all_ids_set)}')
+print(f'Implemented control IDs: {len(all_implemented_ids_set)}')
+print(f'Finish rate: {len(all_implemented_ids_set) / len(all_ids_set) * 100:.4}%')
+
+print(sorted(all_ids_set - all_ids_set.intersection(all_implemented_ids_set)))
+
+print(f'To be implemented control IDs: {len(all_ids_set - all_ids_set.intersection(all_implemented_ids_set))}')
